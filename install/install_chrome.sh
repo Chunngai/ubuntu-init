86b6ff42ec685df70e1c9ab10e840bf5a83d1feb
Chunngai <mikumikku3939@icloud.com>
2020-07-14 15:55:32 +0800

    install chrome: support adding bookmarks

--- install/install_chrome.sh
+++ install/install_chrome.sh
@@ -20,4 +20,23 @@ sudo apt-get -y install google-chrome-stable
 # Modifies google-chrome.list in /etc/apt/sources.list.d to avoid repeated configuration.
 sudo cp google-chrome.list /etc/apt/sources.list.d/google-chrome.list
 
+# Copies the bookmark to ~/.config/google-chrome/Default/, if provided.
+eval set -- $(getopt -o b: -- "$@")
+
+while [ -n $1 ]
+do
+	case $1 in
+		-b)
+			sudo cp $2 ~/.config/google-chrome/Default/Bookmarks
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+
+	shift
+done
+
 whereis google-chrome-stable
